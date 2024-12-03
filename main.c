18c2bb22a3ca72d5fd87fb08a528943df5892982
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2024-12-03 13:39:36 -0300

    Terminamos el main

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #include "include/agencyTreeADT.h"
 #include "include/diffTreeADT.h"
+#include "include/validIDADT.h"
 #include "include/queries.h"
 #include "include/readData.h"
-#include "include/validIDADT.h"
-#include "include/lib.h"
 #include "include/formats.h"
-#include <stdio.h>
-#include <errno.h>
+#include "include/errorManagement.h"
+#include <stdlib.h>
+
+#define EXPECTEDARGS 3
+typedef void * (*newADT)(void);
+void * createNew(newADT);
 
 int main(int argc, char ** argv) {
-    validIDADT validIDs = newValidID();
-    if (errno == ENOMEM) {
-        exit(1);
+    if (argc != EXPECTEDARGS) {
+        fprintf(stderr, "Invalid arguments, expected: %d, recieved: %d", EXPECTEDARGS, argc);
+        exit(EXIT_FAILURE);
     }
+    validIDADT validIDs = createNew((newADT)newValidID);
     readInfractions(argv[INFRACTIONARG], validIDs);
-    agencyTreeADT agencyManagement = newAgencys();
-    diffTreeADT diffAmounts = newDiff();
+    agencyTreeADT agencyManagement = createNew((newADT)newAgencys);
+    diffTreeADT diffAmounts = createNew((newADT)newDiff);
     readTicketCSV(argv[TICKETARG], validIDs, agencyManagement, diffAmounts);
     runMultithreadedQueries(validIDs, diffAmounts, agencyManagement);
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+void * createNew(newADT newFunction) {
+    void * ADT = newFunction();
+    checkErrno();
+    return ADT;
 }
 
