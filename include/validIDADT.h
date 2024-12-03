abb5ded7cc36d2c62919e6d934ed13c7a3bf4ada
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2024-12-03 16:34:10 -0300

    Agrego comentarios

--- include/validIDADT.h
+++ include/validIDADT.h
@@ -10,9 +10,16 @@ typedef struct validIDCDT * validIDADT;
 validIDADT newValidID(void);
 
 // Adds a valid ID, if the ID is added successfully returns ture else returns false
+// @param validIDs: ADT to store and access validIDs
+// @param id: Infraction id to add
+// @param description: Description from infraction of id:id
 bool addID(validIDADT validIDs, unsigned char id, char * description);
 
+// POSIBLE CAMBIO: ID Y DESCRIPTION METERLOS EN UN STRUCT
+
 // Validates ID
+// @param validIDs: ADT to store and access validIDs
+// @param id: id we want to validate
 bool isValidID(validIDADT validIDs, unsigned char id);
 
 #endif
