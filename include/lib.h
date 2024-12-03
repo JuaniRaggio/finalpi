a9a0a7cc8aa8873be699447d863417fa3a920647
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2024-12-03 16:29:10 -0300

    Agrego comentarios

--- include/lib.h
+++ include/lib.h
@@ -4,15 +4,26 @@
 #define __LIB_H__
 
 // Copys until a separator is found or targetDim is reached
-char * myStrcpy(char * target, size_t targetDim, char * source, char * separators);
+// @param target: Copys into target
+// @param targetDim: target dimension
+// @param source: Copys from source
+// @param separators
+char * myStrcpy(char * target, size_t targetDim,const char * source, char * separators);
 
 // Resizes pointer and fills with zero
+// @param ptr: Pointer to resize
+// @param sizePtr: size of Pointer type
+// @param oldSize: Old length of ptr
+// @param newSize: New length of ptr
 void reCalloc(void ** ptr, size_t sizePtr, size_t oldSize, size_t newSize);
 
 // Reads line and appends it to s
+// @param s: Vector of maxLength characters
+// @param maxLength: Length of s
 unsigned int getLine(char s[], unsigned int maxLength);
 
 // Deletes extra whitespaces
+// @param str: String to trim
 void trimWhitespace(char *str);
 
 #endif
--- include/processData.h
+++ include/processData.h
@@ -5,10 +5,22 @@
 #ifndef __PROCESSDATA_H__
 #define __PROCESSDATA_H__
 
+// Inserts at diffAmount and agencyTree a valid Ticket
+// @param diffAmount: ADT of stored diffAmounts
+// @param agencyTree: ADT of stored agencyTree
+// @param data: Ticket to insert
 void insertBothTrees(diffTreeADT diffAmount, agencyTreeADT agencyTree, TTicket data);
 
+// Makes valid the infraction at line -> Adds it to validIDADT
+// @param line: String which represents a read line
+// @param infractions: ADT of valid infractions
 void processInfraction(char *line, validIDADT infractions);
 
+// Adds ticket to agencyTreeADT and to diffTreeADT
+// @param line: String which represents a read line
+// @param infractions: ADT of valid infractions
+// @param agencys: ADT of stored agencys
+// @param diffs: ADT of stored diffAmounts
 void processTicket(char *line, validIDADT infractions, agencyTreeADT agencys, diffTreeADT diffs);
 
 #endif
